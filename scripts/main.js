27800bd632e01d51f413fdd17bc0da69907caf27
Alun Ward <a.ward@eyedivision.com>
2017-12-19 15:00:51 +0000

    Initial commit of Mr Site site copy

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+// var closeMenu = '<li class="close-menu">Close Menu</li>';
+
+// function mobileSetUp() {
+
+// 	$('nav.main-menu ul li.close-menu', '.main-header').remove();
+
+// 	if( $(window).outerWidth() < 600 ){
+		
+// 		if( $('nav.main-menu ul li.close-menu', '.main-header').length === 0 ){
+// 			$('nav.main-menu ul', '.main-header').prepend(closeMenu);
+// 		}
+		
+// 		$('.close-menu').on('click', function(){
+// 			$('nav.main-menu', '.main-header').removeClass('show');
+// 			//$('.intro').css('position', 'relative ');
+// 		});
+// 	}	
+// }
+
+// mobileSetUp();
+
+// $(window).on('resize', function(){
+// 	mobileSetUp();
+// });
+
+// $('.mobile-menu').on('click', function() {
+// 	$('nav.main-menu', '.main-header').addClass('show');
+// });
+
